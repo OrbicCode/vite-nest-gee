d0bb3fb71fec8423c331e7e5cc40a12f3fadab1c
James O'Kane <orbiccode@gmail.com>
2026-06-17 16:19:37 +0100

    test connection setup

--- backend/src/gee/gee.controller.ts
+++ backend/src/gee/gee.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { GeeService } from './gee.service';
+import * as path from 'path';
 
 @Controller('gee')
 export class GeeController {
@@ -17,5 +18,28 @@ export class GeeController {
     );
     const data = await response.json();
     return data;
+  }
+
+  @Get('test-connection')
+  async testConnection() {
+    var ee = require('@google/earthengine');
+    var privateKey = require(
+      path.join(__dirname, '../../src/gee/.private-key.json'),
+    );
+
+    ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function (err) {
+      if (err) {
+        console.error('Authentication error:', err);
+        return;
+      }
+      console.log('Authentication successful');
+    });
+
+    function runAnalysis() {
+      ee.initialize(null, null, null, null, null, 'molten-temple-441717-i6');
+
+      console.log('Running analysis...');
+      // Your analysis code here
+    }
   }
 }
