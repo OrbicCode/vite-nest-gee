43be9c8c6420dde7d37c87fcb10797bfa6c85a67
James O'Kane <orbiccode@gmail.com>
2026-06-17 15:08:49 +0100

    dummy route

--- backend/src/gee/gee.controller.ts
+++ backend/src/gee/gee.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GeeService } from './gee.service';
 
 @Controller('gee')
 export class GeeController {
   constructor(private readonly geeService: GeeService) {}
+
+  @Get()
+  async test() {
+    return 'gee route';
+  }
+
+  @Get('dummy')
+  async getDummyData() {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/1',
+    );
+    const data = await response.json();
+    return data;
+  }
 }
