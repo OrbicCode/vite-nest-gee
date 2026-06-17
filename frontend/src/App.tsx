aa9e555c739c58428b673223929f2880307041ab
James O'Kane <orbiccode@gmail.com>
2026-06-17 16:18:43 +0100

    maplibre install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const map = new maplibregl.Map({
+      container: "map", // container id
+      style: "https://demotiles.maplibre.org/globe.json", // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 3, // starting zoom
+    });
+  }, []);
+
   return (
     <main>
-      <h1>Home</h1>
+      <div id='map' className='map'></div>
     </main>
   );
 }
